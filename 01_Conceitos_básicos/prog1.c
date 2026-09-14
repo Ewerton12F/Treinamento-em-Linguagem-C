56f6db4cdfbcd89d21d46452909babb50b04e299
Ewerton Freitas <ewerton12freitas@gmail.com>
2026-09-14 10:55:03 -0300

    comments

--- 01_Conceitos_básicos/prog1.c
+++ 01_Conceitos_básicos/prog1.c
@@ -1,8 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main()
-{
-	printf("Primeiro programa\n");
-	system("PAUSE");
+/*Esse programa mostra o uso de comentários em várias linhas *
+ * e mostra também o uso de comentários em uma única linha   *
+ *															 *
+ * 						Primeiro programa					 *
+ *************************************************************
+*/
+/* Progl.C */
+#include <stdio.h> /* Para printf()) */
+#include <stdlib.h> /* Para system() */
+int main() /* Função main */
+{ /* início do corpo da função main */
+	printf("Primeiro programa\n"); /* Chamada a função printf */
+	system("PAUSE"); /* Chamada a função system */
 	return 0;
-}
+} /* Fim do corpo da função main */
